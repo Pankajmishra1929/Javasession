138032b249f56a8cc2214055c0ed573ea35828ad
Pankaj Mushra <mpankajmishra1929@gmail.com>
2023-12-18 23:43:49 +0530

    updated as per review comment

--- src/BuilderPattern/DemoJava.java
+++ src/BuilderPattern/DemoJava.java
@@ -4,7 +4,13 @@ public class DemoJava {
 
 	public static void main(String[] args) {
 		System.out.println("splitting branch");
-
+		
+		
 	}
+	
+	public static void addCart() {
+		System.out.println("cart is added");
+	}
+
 
 }
